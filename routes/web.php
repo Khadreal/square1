a90bbaf4a5712132ffa48370719e5df6ae0491d1
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2022-01-13 04:18:02 +0100

    :closed_lock_with_key: Add authentication route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,10 @@ Route::get('/', function () {
 
 
 /** Post related controllers */
-Route::get('posts', [ PostController::class, 'index' ]);
+Route::get('posts', [ PostController::class, 'index' ])->name('home');
+
+// Auth controller
+Route::get('login', [ AuthController::class, 'login'])->name('login');
+Route::post('login', [ AuthController::class, 'loginProcess'])->name('auth.login');
+Route::get('register', [ AuthController::class, 'register'] )->name('register');
+Route::post('register', [AuthController::class, 'registerProcess'])->name('auth.register');
