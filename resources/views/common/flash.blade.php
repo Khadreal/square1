e75504f305d73dc8544176908c6392298fe8e049
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2022-01-13 06:01:08 +0100

    Add error template

---
+++ resources/views/common/flash.blade.php
@@ -0,0 +1,9 @@
+@foreach(['danger' => 'Error', 'success' => 'Success', 'info' => 'Information', 'warning' => 'Important Warning'] as $flashMesg => $label)
+    @if (session($flashMesg))
+        <div class="alert alert-{{$flashMesg}} alert-dismissible fade show" role="alert">
+            <strong>{{$label}}!</strong> {{ session($flashMesg) }}
+            <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+            </button>
+        </div>
+    @endif
+@endforeach
