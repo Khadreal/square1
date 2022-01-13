2956bb90f5f71801d8a7e59676bc5f02e941d1f6
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2022-01-13 16:50:13 +0100

    Add command for external post

---
+++ app/Console/Commands/ExternalPostCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Console\Command;
+use App\Square1\ExternalPost;
+
+
+class ExternalPostCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'external:post';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to fetch external post';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $posts = (new ExternalPost())->savePostToDB();
+        if(!$posts){
+            Log::info('External post run at '. now() . ' was not completed');
+        }
+    }
+}
